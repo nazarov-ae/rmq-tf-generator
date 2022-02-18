d504581e357493b6477bc49aeb986823a5a58620
spice0xff <spice0xff@gmail.com>
2022-02-18 13:50:14 +0700

    prospector message fix

--- tests/test_rmq_tf_generator.py
+++ tests/test_rmq_tf_generator.py
@@ -44,9 +44,6 @@ def create_fake_fs(file_struct):
 def get_success_cases_dirs():
     for case_dir in os.listdir(SUCCESS_CASES_DIR):
         yield case_dir
-    # yield 'one_to_one'
-    # yield 'group_queue'
-    # yield 'double_two'
 
 
 @pytest.mark.parametrize('case_dir', get_success_cases_dirs())
@@ -64,167 +61,167 @@ def test_build_config(case_dir, fs_plus):  # noqa: redefined-outer-name
     assert actual_config == excepted_config
 
 
-# @pytest.mark.parametrize('fs_struct, exception_match', [
-#     (
-#         {
-#             'service1': {
-#                 'produces': {
-#                     'entity1.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service1\"}"
-#             },
-#             'service2': {
-#                 'produces': {
-#                     'entity1.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service2\"}"
-#             },
-#         },
-#         ['entity1']
-#     ),
-#     (
-#         {
-#             'service1': {
-#                 'produces': {
-#                     'entity1.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service1\"}"
-#             },
-#             'service2': {
-#                 'produces': {
-#                     'entity1.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service2\"}"
-#             },
-#             'service3': {
-#                 'produces': {
-#                     'entity2.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service3\"}"
-#             },
-#             'service4': {
-#                 'produces': {
-#                     'entity2.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service4\"}"
-#             },
-#         },
-#         ['entity1', 'entity2']
-#     )
-# ])
-# def test_multiple_producer_error(fs_plus, fs_struct, exception_match):  # noqa: redefined-outer-name
-#     create_fake_fs(fs_struct)
-#
-#     with pytest.raises(MultipleProducerError, match=str(exception_match)):
-#         ConfigBuilder(SERVICES_DIR).build()
+@pytest.mark.parametrize('fs_struct, exception_match', [
+    (
+        {
+            'service1': {
+                'produces': {
+                    'entity1.json': '',
+                },
+                "service.json": "{\"account\": \"service1\"}"
+            },
+            'service2': {
+                'produces': {
+                    'entity1.json': '',
+                },
+                "service.json": "{\"account\": \"service2\"}"
+            },
+        },
+        ['entity1']
+    ),
+    (
+        {
+            'service1': {
+                'produces': {
+                    'entity1.json': '',
+                },
+                "service.json": "{\"account\": \"service1\"}"
+            },
+            'service2': {
+                'produces': {
+                    'entity1.json': '',
+                },
+                "service.json": "{\"account\": \"service2\"}"
+            },
+            'service3': {
+                'produces': {
+                    'entity2.json': '',
+                },
+                "service.json": "{\"account\": \"service3\"}"
+            },
+            'service4': {
+                'produces': {
+                    'entity2.json': '',
+                },
+                "service.json": "{\"account\": \"service4\"}"
+            },
+        },
+        ['entity1', 'entity2']
+    )
+])
+def test_multiple_producer_error(fs_plus, fs_struct, exception_match):  # noqa: redefined-outer-name
+    create_fake_fs(fs_struct)
+
+    with pytest.raises(MultipleProducerError, match=str(exception_match)):
+        ConfigBuilder(SERVICES_DIR).build()
 
 
-# @pytest.mark.parametrize('fs_struct, exception_match', [
-#     (
-#         {
-#             'service1': {
-#                 'consumes': {
-#                     'entity1.json': '',
-#                 },
-#             },
-#             "service.json": "{\"account\": \"service1\"}"
-#         },
-#         ['entity1']
-#     ),
-#     (
-#         {
-#             'service1': {
-#                 'produces': {
-#                     'entity1.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service1\"}"
-#             },
-#             'service2': {
-#                 'consumes': {
-#                     'entity2.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service2\"}"
-#             },
-#         },
-#         ['entity2']
-#     ),
-#     (
-#         {
-#             "service1": {
-#                 "produces": {
-#                     "entity1.json": '',
-#                 },
-#                 "service.json": "{\"account\": \"service1\"}"
-#             },
-#             "service2": {
-#                 "consumes": {
-#                     "queue": {
-#                         "entity1.json": '',
-#                         "entity2.json": '',
-#                     }
-#                 },
-#                 "service.json": "{\"account\": \"service2\"}"
-#             }
-#         },
-#         ['entity2']
-#     )
-# ])
-# def test_producer_missing_error(fs_struct, fs_plus, exception_match):  # noqa: redefined-outer-name
-#     create_fake_fs(fs_struct)
-#
-#     # TODO: match don't work?
-#     with pytest.raises(ProducerMissingError, match=str(exception_match)):
-#         ConfigBuilder(SERVICES_DIR).build()
-#
-#
-# @pytest.mark.parametrize('fs_struct, exception_match', [
-#     (
-#         {
-#             'service1': {
-#                 'produces': {
-#                     'entity1.json': '',
-#                 },
-#                 'service.json': '',
-#             },
-#             'service2': {
-#                 'consumes': {
-#                     'entity1.json': '',
-#                 },
-#                 'service.json': '',
-#             },
-#         },
-#         ['service1', 'service2', ]
-#     ),
-# ])
-# def test_service_account_not_set_error(fs_struct, fs_plus, exception_match):  # noqa: redefined-outer-name
-#     create_fake_fs(fs_struct)
-#
-#     with pytest.raises(ServiceAccountNotSetError, match=str(exception_match)):
-#         ConfigBuilder(SERVICES_DIR).build()
-#
-#
-# @pytest.mark.parametrize('fs_struct, exception_match', [
-#     (
-#         {
-#             'service1': {
-#                 'produces': {
-#                     'entity1.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service1\"}"
-#             },
-#             'service2': {
-#                 'consumes': {
-#                     'entity1.json': '',
-#                 },
-#                 "service.json": "{\"account\": \"service1\"}"
-#             },
-#         },
-#         ['service1', 'service2', ]
-#     ),
-# ])
-# def test_duplicate_service_account_error(fs_struct, fs_plus, exception_match):  # noqa: redefined-outer-name
-#     create_fake_fs(fs_struct)
-#
-#     with pytest.raises(
-#             DuplicateServiceAccountError, match=str(exception_match)):
-#         ConfigBuilder(SERVICES_DIR).build()
+@pytest.mark.parametrize('fs_struct, exception_match', [
+    (
+        {
+            'service1': {
+                'consumes': {
+                    'entity1.json': '',
+                },
+            },
+            "service.json": "{\"account\": \"service1\"}"
+        },
+        ['entity1']
+    ),
+    (
+        {
+            'service1': {
+                'produces': {
+                    'entity1.json': '',
+                },
+                "service.json": "{\"account\": \"service1\"}"
+            },
+            'service2': {
+                'consumes': {
+                    'entity2.json': '',
+                },
+                "service.json": "{\"account\": \"service2\"}"
+            },
+        },
+        ['entity2']
+    ),
+    (
+        {
+            "service1": {
+                "produces": {
+                    "entity1.json": '',
+                },
+                "service.json": "{\"account\": \"service1\"}"
+            },
+            "service2": {
+                "consumes": {
+                    "queue": {
+                        "entity1.json": '',
+                        "entity2.json": '',
+                    }
+                },
+                "service.json": "{\"account\": \"service2\"}"
+            }
+        },
+        ['entity2']
+    )
+])
+def test_producer_missing_error(fs_struct, fs_plus, exception_match):  # noqa: redefined-outer-name
+    create_fake_fs(fs_struct)
+
+    # TODO: match don't work?
+    with pytest.raises(ProducerMissingError, match=str(exception_match)):
+        ConfigBuilder(SERVICES_DIR).build()
+
+
+@pytest.mark.parametrize('fs_struct, exception_match', [
+    (
+        {
+            'service1': {
+                'produces': {
+                    'entity1.json': '',
+                },
+                'service.json': '',
+            },
+            'service2': {
+                'consumes': {
+                    'entity1.json': '',
+                },
+                'service.json': '',
+            },
+        },
+        ['service1', 'service2', ]
+    ),
+])
+def test_service_account_not_set_error(fs_struct, fs_plus, exception_match):  # noqa: redefined-outer-name
+    create_fake_fs(fs_struct)
+
+    with pytest.raises(ServiceAccountNotSetError, match=str(exception_match)):
+        ConfigBuilder(SERVICES_DIR).build()
+
+
+@pytest.mark.parametrize('fs_struct, exception_match', [
+    (
+        {
+            'service1': {
+                'produces': {
+                    'entity1.json': '',
+                },
+                "service.json": "{\"account\": \"service1\"}"
+            },
+            'service2': {
+                'consumes': {
+                    'entity1.json': '',
+                },
+                "service.json": "{\"account\": \"service1\"}"
+            },
+        },
+        ['service1', 'service2', ]
+    ),
+])
+def test_duplicate_service_account_error(fs_struct, fs_plus, exception_match):  # noqa: redefined-outer-name
+    create_fake_fs(fs_struct)
+
+    with pytest.raises(
+            DuplicateServiceAccountError, match=str(exception_match)):
+        ConfigBuilder(SERVICES_DIR).build()
