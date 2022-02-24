1136c8b6f4739063b1437e83c07e2508da56fa07
spice0xff <spice0xff@gmail.com>
2022-02-24 13:49:49 +0700

    mod package

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with codecs.open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    requirements = [
+        line for line in f.readlines()
+        if line and not line.startswith('#')
+    ]
+
+setuptools.setup(
+    name="rmq-tf-generator",
+    version="0.0.1",
+    author="pik-software",
+    author_email="no-reply@pik-software.ru",
+    description="Util for generate terraform config for rabbitmq",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pik-software/rmq-tf-generator",
+    project_urls={
+        "Bug Tracker": "https://github.com/pik-software/rmq-tf-generator/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
